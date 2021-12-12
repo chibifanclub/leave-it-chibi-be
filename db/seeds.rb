b68c6a41eafcdf0dd225e29fa673573eec051970
Khoi Nguyen <khoinguyen311@gmail.com>
2021-12-11 16:11:24 -0800

    Add search seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Search.create!(item_id: 4, count: 10)
+Search.create!(item_id: 7, count: 3)
+Search.create!(item_id: 1, count: 1)
+Search.create!(item_id: 12, count: 3)
+Search.create!(item_id: 6, count: 7)
+Search.create!(item_id: 3, count: 8)
