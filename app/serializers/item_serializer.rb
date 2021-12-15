df2b99fd36f5cf271f0aec5b1a817a0908abea41
Ida Olson <idaolson@gmail.com>
2021-12-14 19:49:59 -0700

    Add image url for attached images to item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,8 @@
 class ItemSerializer
   include JSONAPI::Serializer
 
-  attributes :name, :description, :toxicity, :treatment, :image
+  attributes :name, :description, :toxicity, :treatment
+  attribute :image do |object|
+    object.image_url
+  end
 end
