a008fd401db951d99ca4ad5c7f3b8a42229d46b2
Khoi Nguyen <khoinguyen311@gmail.com>
2021-12-15 15:13:57 -0800

    Comment out unused code

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -14,12 +14,16 @@ class Api::V1::ItemsController < ApplicationController
 
   # def show
   #   item = Item.find(params[:id])
-  #   if item.search.nil?
-  #     Search.create(item_id: item.id, count: 1)
+  #   if item
+  #     if item.search.nil?
+  #       Search.create(item_id: item.id, count: 1)
+  #     else
+  #       item.increment
+  #     end
+  #     render json: ItemSerializer.new(item)
   #   else
-  #     item.increment
+  #     render json: { response: 'Not Found' }, status: :not_found
   #   end
-  #   render json: ItemSerializer.new(item)
   # end
 
   def top_five
