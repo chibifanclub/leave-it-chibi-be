acfb01c6945e63c2cdc87d3f6c18a4b969694d58
Ida Olson <idaolson@gmail.com>
2021-12-14 19:47:30 -0700

    Add one image attachment to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one :search
+  has_one_attached :image
 
   def self.search_for(search_params)
     where("name ILIKE ?", "%#{search_params}%")
@@ -19,4 +20,16 @@ class Item < ApplicationRecord
       .order(count: :desc)
       .limit(5)
   end
+
+  def image_url
+    if image.attached?
+      image.blob.service_url
+    end
+  end
+  #
+  # def image_url
+  #   if self.image_url.attachment
+  #     self.image_url.attachment.service_url
+  #   end
+  # end
 end
